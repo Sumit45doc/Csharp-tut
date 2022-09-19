958215034a793011537997fd89d1c0c3781fd7ec
Sumit Kumar Sharma <ss8559283@gmail.com>
2022-09-19 21:53:45 +0530

    explicit casting

--- Program.cs
+++ Program.cs
@@ -1,7 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 
-using System;
+// using System;
 
 namespace HelloWorld
 {
@@ -9,16 +9,20 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            int number1 = 10;
-            int number2 = 10;
+            // int number1 = 10;
+            // int number2 = 10;
 
             // variable types
             // int, double, string, char, bool
             // use const to avoid reassigning of variable 
 
-            double num = 10.2D;
-            Console.WriteLine(number1 + number2);
-            Console.WriteLine(num);
+            // double num = 10.2D;
+            // Console.WriteLine(number1 + number2);
+            // Console.WriteLine(num);
+            float explicitNum = 10.5F;
+            int myInt = (int) explicitNum;
+            Console.WriteLine(myInt);
+
         }
     }
 }
