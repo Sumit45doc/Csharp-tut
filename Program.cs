e4cd6ca26ff94ea9eadc3b2cf4ac658439ce783f
Sumit Kumar Sharma <ss8559283@gmail.com>
2022-09-19 21:38:02 +0530

    variable

--- Program.cs
+++ Program.cs
@@ -7,11 +7,18 @@ namespace HelloWorld
 {
     class Program
     {
-        static void Main(string[] args){
-            Console.WriteLine("Hello ladies !!!");
-            Console.WriteLine("This is next line");
-            Console.Write("Came Line should apper ");
-            Console.Write("Came Line should apper ");
+        static void Main(string[] args)
+        {
+            int number1 = 10;
+            int number2 = 10;
+
+            // variable types
+            // int, double, string, char, bool
+            // use const to avoid reassigning of variable 
+
+            double num = 10.2D;
+            Console.WriteLine(number1 + number2);
+            Console.WriteLine(num);
         }
     }
 }
